95530c3dd7e98080bc23d3f54096203ab875a01c
Adam0429 <872490934@qq.com>
2019-02-04 20:18:41 +0800

    upload new label_data

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -10,6 +10,7 @@ import numpy as np
 import spacy
 from gensim.models import Word2Vec
 from tqdm import tqdm
+import math
 # nltk.download()
 
 def review_to_words(review_text):
@@ -28,30 +29,79 @@ def review_to_words(review_text):
 	#
 	# 4. In Python, searching a set is much faster than searching
 	#   a list, so convert the stop words to a set
-	stops = set(stopwords.words("english"))				  
+	stopwords = set(stopwords.words("english"))
+	stopwords = nltk.corpus.stopwords.words('english')
+	stopwords.append('would')
+	stopwords.append('kmh')
+	stopwords.append('mph')
+	stopwords.append('  ')
+	stopwords.append('Reuters')				  
 	# 
 	# 5. Remove stop words
-	meaningful_words = [w for w in words if not w in stops]   
+	meaningful_words = [w for w in words if not w in stopwords]   
 	#
 	# 6. Join the words back into one string separated by space, 
 	# and return the result.
 	return(' '.join(meaningful_words))
 
+def PS(w,contents,labels):
+	N = len(contents)
+	pos_count = 0
+	neg_count = 0
+	w_pos_count = 0
+	w_neg_count = 0
+	w_count = 0
+	for index in range(0,len(labels)):
+		if w in contents[index]:
+			w_count += 1
+		label = labels[index].split(',')
+		if '暂无数据' in label:
+			continue
+		elif '1' in label and '0' not in label:
+			pos_count += 1
+			if w in contents[index]:
+				w_pos_count += 1
+			continue
+		elif '0' in label and '1' not in label:
+			neg_count += 1
+			if w in contents[index]:
+				w_neg_count += 1
+
+	freq_pos = pos_count/N 
+	freq_neg = neg_count/N
+	freq_w_pos = w_pos_count/N
+	freq_w_neg = w_neg_count/N
+	freq_w = w_count/N
+
+	if freq_w_pos*N == 0:
+		PMI_w_pos = 0
+	else:
+		PMI_w_pos = np.log(freq_w_pos*N/freq_w*freq_pos)
+	if freq_w_neg*N == 0:
+		PMI_w_neg = 0
+	else:
+		PMI_w_neg = np.log(freq_w_neg*N/freq_w*freq_neg)
+	return PMI_w_pos - PMI_w_neg
 
 
-workbook = xlrd.open_workbook(r'/Users/wangfeihong/Desktop/Dynamic-Financial-News-Collection-and-Analysis/data/data2.xls')
+
+workbook = xlrd.open_workbook(r'/Users/wangfeihong/Desktop/Dynamic-Financial-News-Collection-and-Analysis/data/data2_label.xls')
 sheet = workbook.sheet_by_index(0)
-contents = sheet.col_values(4)[1:]
+contents = sheet.col_values(1)[1:]
+labels = sheet.col_values(3)[1:]
 nlp = spacy.load('en')
+len_word = 0
 tokens = []
 sentences = []
 for content in tqdm(contents):
-	doc = nlp(content)
-	sents = list(doc.sents)   # 分解为句子
-	for sent in sents:
-		sentences.append(str(sents))
-		token=list(map(str,sent))
-		tokens.append(token)
+	doc = content.split('.*?\.(?=\s+(?:|$))')
+	print(doc)
+	input()
+	# sents = list(doc.sents)   # 分解为句子
+	# for sent in sents:
+	# 	sentences.append(str(sents))
+	# 	token=list(map(str,sent))
+	# 	tokens.append(token)
 
 model = Word2Vec(sentences = tokens,min_count = 2)
 bag_of_keywords = set(['rise','drop','fall','gain','surge','shrink','jump','slump','surge'])
@@ -76,11 +126,12 @@ bag_of_keywords = np.array(list(bag_of_keywords))
 bok_tfidf = TfidfVectorizer(lowercase = False, min_df = 1, vocabulary=bag_of_keywords)
 X_bok_tfidf = bok_tfidf.fit_transform(sentences)
 X_bok_tfidf = X_bok_tfidf.toarray()
-import IPython
-IPython.embed()
 bok_count = CountVectorizer(lowercase=False,min_df=1,vocabulary=bag_of_keywords)
 X_bok_count = bok_count.fit_transform(sentences)
 X_bok_count = X_bok_count.toarray()
+
+print(PS('rise',contents,labels))
+
 # for row in rows:
 # 	rows[rows.index(row)] = review_to_words(row)
 # vectorizer = CountVectorizer(analyzer = "word",   
